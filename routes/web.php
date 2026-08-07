cfc815d7f8620a17dec6e8272832cf16bcd806e9
muhamad.bachri <muhamad.bachri.ext@sisi.sig.id>
2026-08-07 22:11:48 +0700

    #feat initial laravel-request-docs

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// API Documentation
+Route::get('/docs', function () {
+    return view('request-docs::index');
+})->name('api.docs');
